deb6a3fcb5449d23f5d9630a98e8f07282fb5a4f
Milen Andreev <104061865+thedopemanMilen@users.noreply.github.com>
2023-06-14 10:47:58 +0300

    Add files via upload

---
+++ HOSANNA NEBESNA/New folder/src/Model/CircleShape.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+
+namespace Draw.src.Model
+{
+    internal class CircleShape : Shape
+    {
+        public CircleShape(RectangleF rect) : base(rect)
+        {
+        }
+
+        public CircleShape(CircleShape circle) : base(circle)
+        {
+        }
+
+        public override bool Contains(PointF point)
+        {
+            if (base.Contains(point))
+                // Проверка дали е в обекта само, ако точката е в обхващащия правоъгълник.
+                // В случая на правоъгълник - директно връщаме true
+                return true;
+            else
+                // Ако не е в обхващащия правоъгълник, то неможе да е в обекта и => false
+                return false;
+        }
+
+
+        public override void DrawSelf(Graphics grfx)
+        {
+            base.DrawSelf(grfx);
+
+            grfx.FillEllipse(new SolidBrush(FillColor), Rectangle);
+            grfx.DrawEllipse(Pens.Black, Rectangle);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
